4391db5cc12ac9d790a48b5b00ea426c2aaf79e7
m26unkwn <shukla9915294376@gmail.com>
2021-01-26 15:47:17 +0530

    added

---
+++ script.js
@@ -0,0 +1,20 @@
+var door_1=document.getElementById('door1');
+
+var door_2=document.getElementById('door2');
+
+var door_3=document.getElementById('door3');
+
+var robot_path="./assets/robot.svg"
+
+door_1.onclick=()=>{
+    door_1.src=robot_path
+}
+
+door_2.onclick=()=>{
+    door_2.src=robot_path
+}
+
+door_3.onclick=()=>{
+    door_3.src=robot_path
+}
+
