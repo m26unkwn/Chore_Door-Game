6a23fa7bdfbdf1c62fa41dfbcd538888fae75fcc
m26unkwn <shukla9915294376@gmail.com>
2021-01-27 12:07:21 +0530

    Added some Basic Logic

--- script.js
+++ script.js
@@ -1,24 +1,84 @@
-var door_1=document.getElementById('door1');
+let door_1=document.getElementById('door1');
 
-var door_2=document.getElementById('door2');
+let door_2=document.getElementById('door2');
 
-var door_3=document.getElementById('door3');
+let door_3=document.getElementById('door3');
 
-var robot_path="./assets/robot.svg"
+let robot_path="./assets/robot.svg"
 
-var beachPath="./assests/beach.svg"
+let beachPath="./assets/beach.svg"
 
-var spacePath="./assets/space.svg"
+let spacePath="./assets/space.svg"
+
+let numClosedDoor=3
+
+let openDoor0;
+let openDoor1;
+let openDoor2;
+
+let startButton=document.getElementById('start')
+
+let closedDoorPath="./assets/closed_door.svg"
+
+const isClickd=(door)=>{
+    if (door.src===closedDoorPath){
+        return true
+    }
+    else{
+        return false
+    }
+
+}
+
+const playDoor=(door)=>{
+    numClosedDoor--;
+    if (numClosedDoor===0){
+        gameOver()
+    }
+    else{
+        gameOver()
+    }
+}
+
+const randomDoorGenerator=()=>{
+
+        let choreDoor = Math.floor(Math.random() * numClosedDoor);
+
+        if (choreDoor === 0) {
+                openDoor0 = robot_path;
+                openDoor1 = beachPath;
+                openDoor2 = spacePath;
+              } else if (choreDoor === 1) {
+                openDoor1 = robot_path;
+                openDoor0 = beachPath;
+                openDoor2 = spacePath;
+              } else {
+                openDoor2 = robot_path;
+                openDoor0 = beachPath;
+                openDoor1 = spacePath;
+              }
+}
+
 
 door_1.onclick=()=>{
-    door_1.src=robot_path
+    if(!isClickd(door_1)){
+    door_1.src=openDoor0
+    playDoor(door_1)
+    }
 }
 
 door_2.onclick=()=>{
-    door_2.src=beachPath
+    if(!isClickd(door_2)){
+        door_2.src=openDoor0
+        playDoor(door_2)
+        }
 }
 
 door_3.onclick=()=>{
-    door_3.src=spacePath
+    if(!isClickd(door_3)){
+        door_3.src=openDoor0
+        playDoor(door_3)
+        }
 }
 
+randomDoorGenerator()
