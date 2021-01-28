332c06fc212669f8d94d82f93185cd5081f408a4
m26unkwn <shukla9915294376@gmail.com>
2021-01-28 21:44:37 +0530

    Solved a problem with Javascript

--- script.js
+++ script.js
@@ -1,84 +1,141 @@
-let door_1=document.getElementById('door1');
+ let door_1=document.getElementById('door1');
 
 let door_2=document.getElementById('door2');
 
 let door_3=document.getElementById('door3');
 
-let robot_path="./assets/robot.svg"
+let robotPath = "https://content.codecademy.com/projects/chore-door/images/robot.svg";
+let beachPath = "https://content.codecademy.com/projects/chore-door/images/beach.svg";
+let spacePath = "https://content.codecademy.com/projects/chore-door/images/space.svg";
+let closedDoorPath = "https://content.codecademy.com/projects/chore-door/images/closed_door.svg"
 
-let beachPath="./assets/beach.svg"
+let numClosedDoors=3;
 
-let spacePath="./assets/space.svg"
-
-let numClosedDoor=3
-
-let openDoor0;
 let openDoor1;
 let openDoor2;
+let openDoor3;
 
-let startButton=document.getElementById('start')
+let startButton=document.getElementById('start');
 
-let closedDoorPath="./assets/closed_door.svg"
+let currentPlaying= true;
 
-const isClickd=(door)=>{
-    if (door.src===closedDoorPath){
-        return true
-    }
-    else{
-        return false
-    }
+let score = 0;
+let highScore = 0;
+let currentStreak = document.getElementById('score-number');
+let bestStreak = document.getElementById('high-score-number');
+currentStreak.innerHTML = score;
+bestStreak.innerHTML = highScore;
 
-}
-
-const playDoor=(door)=>{
-    numClosedDoor--;
-    if (numClosedDoor===0){
-        gameOver()
-    }
-    else{
-        gameOver()
+function isClicked(door) {
+    if (door.src == closedDoorPath) {
+        return false;
+    } else {
+        return true;
     }
 }
+  
+
+
+function isBot(door) {
+    if (door.src === robotPath) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+
+function playDoor(door) {
+    numClosedDoors--;
+    if (numClosedDoors === 0) {
+        gameOver('win');
+    }
+    else if (isBot(door)) {
+        gameOver('lose');
+    }
+}
+
+
 
 const randomDoorGenerator=()=>{
 
-        let choreDoor = Math.floor(Math.random() * numClosedDoor);
+        let choreDoor = Math.floor(Math.random() * numClosedDoors);
 
         if (choreDoor === 0) {
-                openDoor0 = robot_path;
+                openDoor1 = robotPath;
+                openDoor2 = beachPath;
+                openDoor3 = spacePath;
+              } else if (choreDoor === 1) {
+                openDoor2 = robotPath;
+                openDoor1 = beachPath;
+                openDoor3 = spacePath;
+              } else {
+                openDoor3 = robotPath;
                 openDoor1 = beachPath;
                 openDoor2 = spacePath;
-              } else if (choreDoor === 1) {
-                openDoor1 = robot_path;
-                openDoor0 = beachPath;
-                openDoor2 = spacePath;
-              } else {
-                openDoor2 = robot_path;
-                openDoor0 = beachPath;
-                openDoor1 = spacePath;
               }
 }
 
 
 door_1.onclick=()=>{
-    if(!isClickd(door_1)){
-    door_1.src=openDoor0
-    playDoor(door_1)
+    if(currentPlaying ){
+    door_1.src=openDoor1;
+    playDoor(door_1);
     }
 }
 
 door_2.onclick=()=>{
-    if(!isClickd(door_2)){
-        door_2.src=openDoor0
-        playDoor(door_2)
+    if(currentPlaying ){
+        door_2.src=openDoor2;
+        playDoor(door_2);
         }
 }
 
 door_3.onclick=()=>{
-    if(!isClickd(door_3)){
-        door_3.src=openDoor0
-        playDoor(door_3)
-        }
+    if(currentPlaying && !isClicked(door_3)){
+        door_3.src=openDoor3;
+        playDoor(door_3);
+    }
 }
 
-randomDoorGenerator()
+startButton.onclick = () => {
+    startRound();
+}
+
+
+const gameOver=(status)=>{
+    if (status==='win'){
+    startButton.innerHTML="You win, Play Again!";
+    getYourScore();
+    }
+    else {
+        startButton.innerHTML="You lose, Play Again";
+        score=0
+        currentStreak.innerHTML = score;
+    }
+    currentPlaying=false;
+}
+
+const startRound=()=>{
+  door_1.src = closedDoorPath;
+  door_2.src = closedDoorPath;
+  door_3.src = closedDoorPath;
+  numClosedDoors = 3;
+  currentlyPlaying = true;
+  startButton.innerHTML = 'Good luck!';
+  randomDoorGenerator();
+}
+
+const getYourScore = () => {
+    score++;
+    currentStreak.innerHTML = score;
+    if (score > highScore) {
+      highScore = score;
+      bestStreak.innerHTML = highScore;
+    }
+  }
+
+startRound();
+
+
